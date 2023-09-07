9446bbc27a2f81ba0d838f39d0042de333d45868
millerandrew1 <millerandrew1@ufl.edu>
2023-09-07 15:50:14 -0400

    Test

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var requestHandler = function(request, response) {
   /*Investigate the request object. 
     You will need to use several of its properties: url and method
   */
+ console.log('JDJDJ');
     response.end('Request received!');
 
   /*
