6055bff5b431c4bb99925639e78d76f970222ced
millerandrew1 <millerandrew1@ufl.edu>
2023-09-08 14:43:19 -0400

    test

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var requestHandler = function(request, response) {
     You will need to use several of its properties: url and method
   */
   var parsedURL = url.parse(request.url);
+  console.log('hhh')
     response.write('Request received!');
     response.end('ddd');
 
